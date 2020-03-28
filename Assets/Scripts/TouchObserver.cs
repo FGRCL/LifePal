f2e3b7c355ef1a8bb0ff6a0c6c0967f0d12ba29b
François <Fgrclaberge@gmail.com>
2020-03-27 23:49:38 -0400

    Initial commit

---
+++ Assets/Scripts/TouchObserver.cs
@@ -0,0 +1,3 @@
+public interface TouchObserver{
+    public void notify();
+}
---
+++ Assets/Scripts/TouchSubject.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchSubject : MonoBehaviour
+{
+    private ArrayList<TouchObserver> observers;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began){
+            foreach(TouchObserver observer in observers){
+                observer.notify();
+            }
+        }
+    }
+
+    public void Subscribe(TouchObserver observer){
+
+    }
+}
